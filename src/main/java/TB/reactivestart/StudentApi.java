1b8ab254ce697d9ae61c25ff4bcb9cebd1786541
Tomasz Borowski <bortom112233@gmail.com>
2023-01-15 16:27:19 +0100

    reactive post method to hardcoded Data

--- src/main/java/TB/reactivestart/StudentApi.java
+++ src/main/java/TB/reactivestart/StudentApi.java
@@ -1,9 +1,15 @@
 package TB.reactivestart;
 
 import TB.reactivestart.model.Student;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
 
 @RestController
 public class StudentApi {
@@ -17,8 +23,13 @@ public class StudentApi {
         );
     }
 
-    @GetMapping
+    @GetMapping(produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
     public Flux<Student> get() {
-        return studentList;
+        return studentList.delayElements(Duration.ofSeconds(2));
+    }
+
+    @PostMapping(produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
+    public Flux<Student> add(@RequestBody Student student) {
+        return studentList.mergeWith(Mono.just(student));
     }
 }
